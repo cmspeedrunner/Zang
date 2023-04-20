684a5bb14bedcacdf3049164c93fa399085eee50
Cm <109184310+cmspeedrunner@users.noreply.github.com>
2023-04-20 19:09:07 +0100

    Update zpm.py

--- zpm/zpm.py
+++ zpm/zpm.py
@@ -4,4 +4,3 @@ lib = sys.argv[1]
 os.system("git clone https://github.com/cmspeedrunner/Zang")
 import shutil
 shutil.copy("zang/examples/using/libraries/"+str(lib), str(os.getcwd()))
-os.remove("Zang")
