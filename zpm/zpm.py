706ad11b1bf8629da138b7141237732f5b5fc4cc
Cm <109184310+cmspeedrunner@users.noreply.github.com>
2023-04-20 19:30:44 +0100

    Update zpm.py

--- zpm/zpm.py
+++ zpm/zpm.py
@@ -1,6 +1,18 @@
 import sys
 import os
-lib = sys.argv[1]
-os.system("git clone https://github.com/cmspeedrunner/Zang")
 import shutil
-shutil.copy("zang/examples/using/libraries/"+str(lib), str(os.getcwd()))
+
+lib = sys.argv[1]
+current_dir = os.getcwd()
+
+# Specify the directory name to check
+dir_name = "Zang"
+
+# Create the path to the directory by joining the current directory and the directory name
+dir_path = os.path.join(current_dir, dir_name)
+if os.path.exists(dir_path) and os.path.isdir(dir_path):
+  shutil.copy("zang/examples/using/libraries/"+str(lib), str(os.getcwd()))
+  print("INSTALLED "+str(lib).upper()+" SUCESSFULLY")
+else:
+  os.system("git clone https://github.com/cmspeedrunner/Zang")
+  shutil.copy("zang/examples/using/libraries/"+str(lib), str(os.getcwd()))
