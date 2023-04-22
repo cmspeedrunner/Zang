bb7e591dc41945367498e49429c28b38f9b46295
ZackeryRSmith <72983221+ZackeryRSmith@users.noreply.github.com>
2023-04-22 15:10:31 -0400

    improved ac.history

--- src/shell.py
+++ src/shell.py
@@ -3,50 +3,57 @@ import os
 import readline
 os.system("")
 print("""\033[35m
- /\u001b[34m$$$$$$$$\033[35m                              
-|_____ \u001b[34m$$\033[35m                                 
-     /\u001b[34m$$\033[35m/   /\u001b[34m$$$$$$\033[35m  /\u001b[34m$$$$$$$\033[35m   /\u001b[34m$$$$$$\033[35m 
-    /\u001b[34m$$\033[35m/   |____  \u001b[34m$$\033[35m| \u001b[34m$$\033[35m__  \u001b[34m$$\033[35m /\u001b[34m$$\033[35m__  \u001b[34m$$\033[35m
-   /\u001b[34m$$\033[35m/     /\u001b[34m$$$$$$$\033[35m| \u001b[34m$$\033[35m  \ \u001b[34m$$\033[35m| \u001b[34m$$\033[35m  \ \u001b[34m$$\033[35m
-  /\u001b[34m$$\033[35m/     /\u001b[34m$$\033[35m__  \u001b[34m$$\033[35m| \u001b[34m$$\033[35m  | \u001b[34m$$\033[35m| \u001b[34m$$\033[35m  | \u001b[34m$$\033[35m
- /\u001b[34m$$$$$$$$\033[35m|  \u001b[34m$$$$$$$\033[35m| \u001b[34m$$\033[35m  | \u001b[34m$$\033[35m|  \u001b[34m$$$$$$$\033[35m
-|________/ \_______/|__/  |__/ \____  \u001b[34m$$\033[35m
-                               /\u001b[34m$$\033[35m  \ \u001b[34m$$\033[35m
-                              |  \u001b[34m$$$$$$\033[35m/
+ /\033[34m$$$$$$$$\033[35m                              
+|_____ \033[34m$$\033[35m                                 
+     /\033[34m$$\033[35m/   /\033[34m$$$$$$\033[35m  /\033[34m$$$$$$$\033[35m   /\033[34m$$$$$$\033[35m 
+    /\033[34m$$\033[35m/   |____  \033[34m$$\033[35m| \033[34m$$\033[35m__  \033[34m$$\033[35m /\033[34m$$\033[35m__  \033[34m$$\033[35m
+   /\033[34m$$\033[35m/     /\033[34m$$$$$$$\033[35m| \033[34m$$\033[35m  \ \033[34m$$\033[35m| \033[34m$$\033[35m  \ \033[34m$$\033[35m
+  /\033[34m$$\033[35m/     /\033[34m$$\033[35m__  \033[34m$$\033[35m| \033[34m$$\033[35m  | \033[34m$$\033[35m| \033[34m$$\033[35m  | \033[34m$$\033[35m
+ /\033[34m$$$$$$$$\033[35m|  \033[34m$$$$$$$\033[35m| \033[34m$$\033[35m  | \033[34m$$\033[35m|  \033[34m$$$$$$$\033[35m
+|________/ \_______/|__/  |__/ \____  \033[34m$$\033[35m
+                               /\033[34m$$\033[35m  \ \033[34m$$\033[35m
+                              |  \033[34m$$$$$$\033[35m/
                                \______/ \033[0m 
   \033[35mZang V/0.45
   ------------------
-  DEV: \u001b[32mCm\u001b[35m
-  VERSION: \u001b[32m0.45\u001b[35m
-  GITHUB: \u001b[32mhttps://github.com/cmspeedrunner/zang\u001b[35m
+  DEV: \033[32mCm\033[35m
+  VERSION: \033[32m0.45\033[35m
+  GITHUB: \033[32mhttps://github.com/cmspeedrunner/zang\033[35m
                                          
 """)
 
 history = []
 
+# TODO: function support,
+#       tab auto completion
+#       issue with scrolling down through history
+
 while True:
     try:
-        text = input('\033[35mZang>\033[0m')
-        if text != "ac.history":
-            history.append(text)
-        if text == "ac.history":
-            print(history)
-          # Add input to history list
+        text = input('\033[35mZang> \033[m')
+        
+        if text == 'ac.history':
+            for idx, com in enumerate(history):
+                print(f' \033[34m{idx + 1:>3}.)\033[m {com}')
+            continue
+
+        history.append(text)
     except KeyboardInterrupt:
-        print("\n\033[31m^C\033[0m",end="")
-        exit()
+        exit("\n\033[31m^C\033[m")
+    
     if text.strip() == "":
         continue
-
-    if text != "ac.history":
-        result, error = pret.run('<stdin>', text)
     
-
-        if error:
-            print("\033[31m" + error.as_string() + "\033[0m")  # Print error message in red text
-        elif result:
-            if len(result.elements) == 1:
-                print(repr(result.elements[0]))
-            else:
-                print(repr(result))
-
+    print(text)
+    result, error = pret.run('<stdin>', text)
+    
+    if error:
+        history[-1] = f'\033[31m{history[-1]}\033[m'
+        print(f'\033[31m{error.as_string()}\033[m')
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
+    
+    history[-1] = f'\033[32m{history[-1]}\033[m'
