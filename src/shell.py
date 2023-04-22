e9d4844e110f6112705bd02e81aa32945872da17
Cm <109184310+cmspeedrunner@users.noreply.github.com>
2023-04-22 17:28:58 +0100

    Add files via upload

--- src/shell.py
+++ src/shell.py
@@ -1,12 +1,11 @@
 import interpreter as pret
-import readline
 import os
 os.system("")
 print("""\033[35m
  /\u001b[34m$$$$$$$$\033[35m                              
 |_____ \u001b[34m$$\033[35m                                 
      /\u001b[34m$$\033[35m/   /\u001b[34m$$$$$$\033[35m  /\u001b[34m$$$$$$$\033[35m   /\u001b[34m$$$$$$\033[35m 
-    /\u001b[34m$$\033[35m/   |____  \u001b[34m$$| $$\033[35m__  \u001b[34m$$\033[35m /\u001b[34m$$\033[35m__  \u001b[34m$$\033[35m
+    /\u001b[34m$$\033[35m/   |____  \u001b[34m$$\033[35m| \u001b[34m$$\033[35m__  \u001b[34m$$\033[35m /\u001b[34m$$\033[35m__  \u001b[34m$$\033[35m
    /\u001b[34m$$\033[35m/     /\u001b[34m$$$$$$$\033[35m| \u001b[34m$$\033[35m  \ \u001b[34m$$\033[35m| \u001b[34m$$\033[35m  \ \u001b[34m$$\033[35m
   /\u001b[34m$$\033[35m/     /\u001b[34m$$\033[35m__  \u001b[34m$$\033[35m| \u001b[34m$$\033[35m  | \u001b[34m$$\033[35m| \u001b[34m$$\033[35m  | \u001b[34m$$\033[35m
  /\u001b[34m$$$$$$$$\033[35m|  \u001b[34m$$$$$$$\033[35m| \u001b[34m$$\033[35m  | \u001b[34m$$\033[35m|  \u001b[34m$$$$$$$\033[35m
@@ -27,22 +26,26 @@ history = []
 while True:
     try:
         text = input('\033[35mZang>\033[0m')
-        history.append(text)  # Add input to history list
+        if text != "ac.history":
+            history.append(text)
+        if text == "ac.history":
+            print(history)
+          # Add input to history list
     except KeyboardInterrupt:
         print("\n\033[31m^C\033[0m",end="")
         exit()
     if text.strip() == "":
         continue
 
-    
-    result, error = pret.run('<stdin>', text)
+    if text != "ac.history":
+        result, error = pret.run('<stdin>', text)
     
 
-    if error:
-        print("\033[31m" + error.as_string() + "\033[0m")  # Print error message in red text
-    elif result:
-        if len(result.elements) == 1:
-            print(repr(result.elements[0]))
-        else:
-            print(repr(result))
+        if error:
+            print("\033[31m" + error.as_string() + "\033[0m")  # Print error message in red text
+        elif result:
+            if len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
 
